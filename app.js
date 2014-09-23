6929d86965fb7543aa8bea1df5a7777c46938a8a
Kevin Washington <kevw12188@gmail.com>
2014-09-22 20:51:54 -0400

    some quick setup

--- app.js
+++ app.js
@@ -1,12 +1,28 @@
 var app = angular.module('app', []);
 
 
-app.controller('dogController',function(){
-	//do stuff
+app.controller('dogController',function($scope){
+	
+	$scope.dogs = [
+		{breed:'pitbull', name:'rocky'},
+		{breed:'golden retriever', name: 'chance'},
+		{breed:'bulldog', name: 'Professor G'}
+	]
+
+	$scope.owners = [
+		{name:'Kevin', age:'26'},
+		{name:'Carl', age:'34'},
+		{name:'Mitchell', age:'22'}
+	]
+
 
 });
 
 
 app.directive('dogDirective',function(){
-	//do stuff
+	return function(scope, element, attrs){
+		element.bind('mouseenter',function(){
+			alert('I am in you');
+		})
+	}
 })
